310635a21c43c81b6f8ff88c21cb4ba1d39189ce
orestis.alagialoglou <orestis.alagialoglou@marinetraffic.com>
2023-05-24 13:16:59 +0300

    Refactor entry point files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MainPage from "./components/MainPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Header from "./components/Header";
+import ServiceModal from "./components/ServiceModal";
+import EvidenceModal from "./components/EvidenceModal";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      {/* Your app code here */}
+      <ServiceModal />
+      <EvidenceModal />
+      <Header />
+      <ReactQueryDevtools initialIsOpen />
+      <MainPage />
+    </QueryClientProvider>
   );
 }
-
 export default App;
